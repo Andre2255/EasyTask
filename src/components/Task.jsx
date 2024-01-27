c7832cff4aeb678a13b5b68315c82b8199f52f25
Andrey Rodriguez <andreyrodcha757@gmail.com>
2024-01-27 12:35:38 -0600

    making details

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,14 +1,14 @@
 
-const Task = ({tarea, setTarea, eliminarTarea}) => {
-   const {nombre, creador, email, fecha, detalle, id} = tarea
+const Task = ({ tarea, setTarea, eliminarTarea }) => {
+    const { nombre, creador, email, fecha, detalle, id } = tarea
 
-   const handleEliminar = ()=>{
-    const respuesta = confirm("¿Desea eliminar esta tareas?")
-    if(respuesta){
-        eliminarTarea(id)
+    const handleEliminar = () => {
+        const respuesta = confirm("¿Desea eliminar esta tareas?")
+        if (respuesta) {
+            eliminarTarea(id)
+        }
+
     }
-
-   }
     return (
         <div className='mx-5 my-4 bg-white shadow-md px-5 py-10 rounded-xl'>
 
@@ -28,10 +28,12 @@ const Task = ({tarea, setTarea, eliminarTarea}) => {
                 Detalle : <span className=' font-normal normal-case'>{detalle}</span>
             </p>
 
-            <div className=" flex justify-between mt-10 px-8">
-                <button className=" py-2 px-10 bg-orange-600 text-white hover:bg-orange-700 font-bold uppercase rounded" onClick={() => setTarea(tarea)} type="button" >Editar</button>
-                <button className=" py-2 px-10 bg-red-600 text-white hover:bg-red-700 font-bold uppercase rounded" onClick={handleEliminar} type="button">Eliminar</button>
+            <div className="flex flex-col  md:flex-row justify-between mt-10  ">
+                <button className="mb-4 md:mb-0 py-2 md:py-2 px-6 md:px-8 lg:px-10 bg-orange-600 text-white hover:bg-orange-700 font-bold uppercase rounded w-full md:w-auto">Editar</button>
+                <button className="py-2 md:py-2 px-6 md:px-8 lg:px-10 bg-red-600 text-white hover:bg-red-700 font-bold uppercase rounded w-full md:w-auto" onClick={handleEliminar} type="button">Eliminar</button>
             </div>
+
+
         </div>
     )
 }
